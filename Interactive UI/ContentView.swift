410ab77fc3f18b0583e36431dd63f02de76225a8
shaya fadel <shayaf28@gmail.com>
2024-07-26 10:03:38 -0400

    Initial Commit

---
+++ Interactive UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Interactive UI
+//
+//  Created by Shaya Fadel on 2024-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Interactive UI/Interactive_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interactive_UIApp.swift
+//  Interactive UI
+//
+//  Created by Shaya Fadel on 2024-07-26.
+//
+
+import SwiftUI
+
+@main
+struct Interactive_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
